65e70b37294e3005dad32fd93ac5243eb1c919bd
Ubuntu <ubuntu@ip-172-31-93-49.ec2.internal>
2019-09-13 17:18:50 +0000

    commit added rectangle program

---
+++ rectangle.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2019 Patrick Gemmell All rights reserved.
+//
+// Created by: Patrick Gemmell
+// Created on: September 12 2019
+// This is the hello world program on CPP
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "The Dimensions of the rectangle are:" << std::endl;
+    std::cout << "5mm x 3mm" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "The Area is:" << (5*3) << std::endl;
+     std::cout << "The Perimeter is:" << (2*(5+3)) << std::endl;
+}
